8c247210acb8b69df1fac3e3ce6b5639afa6dd00
Hemant <134163996+x-state7@users.noreply.github.com>
2024-08-04 20:45:47 +0530

    Add files via upload
    This is the final integration of Spam classifier code

---
+++ mail.py
@@ -0,0 +1,100 @@
+import matplotlib.pyplot as plt
+import pickle
+import streamlit as st
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+import string
+
+ps=PorterStemmer()
+nltk.download('stopwords')
+
+# Download the punkt tokenizer data
+nltk.download('punkt')
+
+
+# def transform_text(text):
+#   # lower casing
+#   text=text.lower()
+#   # tokenizing
+#   text=nltk.word_tokenize(text)
+#
+#   # removing special charaters
+#   y=[]
+#   for i in text:
+#     if i.isalnum():
+#       y.append(i)
+#
+# # removing stop words
+#   text=y[:]
+#   y.clear()
+#   for i in text:
+#     if i not in stopwords.words('english') and i not in string.punctuation:
+#       y.append(i)
+#
+# # converting word into root form
+#   text=y[:]
+#   y.clear()
+#   for i in text:
+#     y.append(ps.stem(i))
+#   return " ".join(y)
+
+# Function to preprocess text
+def transform_text(text):
+    # Convert to lowercase
+    text = text.lower()
+    # Tokenize the text
+    text = nltk.word_tokenize(text)
+    # Remove non-alphanumeric characters
+    y = [i for i in text if i.isalnum()]
+    # Remove stopwords and punctuation
+    y = [i for i in y if i not in stopwords.words('english')]
+    # Stem the words
+    y = [ps.stem(i) for i in y]
+    return " ".join(y)
+
+tfidf=pickle.load(open('vectorizer.pkl', "rb"))
+model=pickle.load(open('model.pkl', "rb"))
+
+uploaded_file = st.file_uploader("Upload a text file", type=["txt"])
+
+
+st.title(" Spam Detector")
+input_sms=st.text_area("Enter your message")
+
+result = None
+
+
+if st.button('predict'):
+    if uploaded_file is not None:
+        input_sms = uploaded_file.read().decode("utf-8")
+        transformed_sms = transform_text(input_sms)
+
+        st.text_area("File Content", input_sms, height=200)
+
+        # Vectorize and predict
+        vector_input = tfidf.transform([transformed_sms])
+        result = model.predict(vector_input)[0]
+
+    elif input_sms:
+        # Preprocess the input text
+        transformed_sms = transform_text(input_sms)
+
+        # Vectorize the input text
+        vector_input = tfidf.transform([transformed_sms])
+
+        # Predict the label
+        result = model.predict(vector_input)[0]
+
+        # Display the result
+    if uploaded_file or input_sms:
+        # Display the result
+        if result == 1:
+            st.header("Spam")
+        else:
+            st.header("Not Spam")
+
+
+
+
+
